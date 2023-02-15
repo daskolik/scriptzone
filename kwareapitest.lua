e49599a37c39d7b2c2867331ec38464945ca6fb3
Mustafa <95919407+daskolik@users.noreply.github.com>
2023-02-16 00:32:03 +0300

    Create kwareapitest.lua

---
+++ kwareapitest.lua
@@ -0,0 +1,14 @@
+print("kware api imported !")
+
+
+local function taTurkishArmy():
+    print("exp1 ")
+end
+
+local function btfTurkishArmy():
+    print("exp1")
+end
+
+
+
+print("kwareapi succesfully complied!")
